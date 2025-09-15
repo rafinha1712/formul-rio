8fd5a792e077dd250be88ece88eaec062090bd81
rafinha1712 <mayer.rafaele@escola.pr.gov.br>
2025-09-15 11:19:51 -0300

    ,

--- script.js
+++ script.js
@@ -4,5 +4,13 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
 
 function mostraPalavrasChave() { 
-  alert("fui clicado!"); 
+  let palavras = texto.split(/\P{L}+/u);
+
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
+
+    const frequencias = contaFrequencias(palavras);
 } 
