e5a3205ddeacde240978fd84f48250bad4768acd
rafinha1712 <mayer.rafaele@escola.pr.gov.br>
2025-09-15 11:16:15 -0300

    lol

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+import { PALAVRAS_RUINS } from "./palavrasRuins.js";
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
